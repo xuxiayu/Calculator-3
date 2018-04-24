bcfa7d98a0e6f2282ed4010d720c7fb4ece19d76
dp200 <34171886+dp200@users.noreply.github.com>
2018-04-23 21:44:02 -0400

    seq of seq finished

---
+++ dhara_seq_of_seq/data_defs.v
@@ -0,0 +1,20 @@
+`define CLK_PERIOD 10
+
+`define DATA_WIDTH 32
+`define INSTR_WIDTH 4
+`define RESP_WIDTH 2
+`define TAG_WIDTH 2
+`define REGISTER_WIDTH 4
+`define DELAY_WIDTH 4
+//ARITHMETIC CMDS
+`define NO_OP  4'b0000
+`define ADD 4'b0001
+`define SUB 4'b0010
+
+//SHIFT CMDS
+`define SHL 4'b0101
+`define SHR 4'b0110
+`define SUCC 2'b01
+`define UNDR_OVRFL 2'b10
+`define INTERNAL_ERR 2'b11
+`define MAX 32'b11111111111111111111111111111111
